e3815d5e87f741d5c020ac84404790e351c2db55
Максим Сухарев <m.sukharev@trekmark.ru>
2024-08-15 16:58:55 +0300

    Рабочая версия деплоя на Github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+import vueLogo from './assets/vue.png'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,17 +10,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+        <a href="https://t.me/VanuWko" target="_blank">
+          <img src={vueLogo} className="logo react" alt="Vue logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>ВАНЯ С ДНЕМ РОЖДЕНИЯ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          тапай хомяка {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
